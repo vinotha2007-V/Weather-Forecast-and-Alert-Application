517a8e15bff3bd5023c4482b5ef70d1ec14f48de
vinotha2007-V <logiravi2002@gmail.com>
2026-05-24 18:01:01 +0530

    Create weather_api.py

---
+++ src/weather_api.py
@@ -0,0 +1,18 @@
+import requests
+from src.config import API_KEY
+
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+def get_weather(city):
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
